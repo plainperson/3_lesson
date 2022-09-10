e563be4cd481f737672a09995305d9c6848a3e5c
plainperson <99728196+plainperson@users.noreply.github.com>
2022-09-10 13:15:50 +0600

    Add files via upload

---
+++ task 6.py
@@ -0,0 +1,8 @@
+
+def int_func():
+    word = input('ВВедите слово маленькими буквами на латинице')
+    return word.capitalize()
+
+
+
+print(int_func())
---
+++ task_5.py
@@ -0,0 +1,10 @@
+n = int(input('Введите числа'))
+s = 0
+
+def my_func(num_string):
+    for i in range(num_string):
+        x = int(input())
+        sum += x
+        return sum(num_string)
+
+print(my_func(num_string))
